0dc5604eaf31e20b2c995761ffee73465f23ef8b
finalburner <hmaidouchamine@gmail.com>
2017-02-08 16:51:23 +0100

    Routing + aff

--- App/www/js/app.js
+++ App/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','myApp' ])
+angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','myApp'])
 
 .factory('socket', function (socketFactory) {
   var mySocket = socketFactory({
@@ -14,6 +14,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
   //mySocket.forward('temp');
   return mySocket
     })
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -37,12 +38,14 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
     .state('app', {
     url: '/app',
     abstract: true,
+    authentificate: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
   })
 
   .state('app.synop', {
     url: '/synop',
+      authentificate: true,
     views: {
       'menuContent': {
         templateUrl: 'templates/synop.html',
@@ -50,9 +53,30 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
       }
     }
   })
+  .state('app.synop.carto', {
+    url: '/carto',
+      authentificate: true,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/carto.html',
+        controller: 'MyCtrl'
+      }
+    }
+  })
+  .state('app.synop.CT', {
+    url: '/CT',
+      authentificate: true,
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/CT.html',
+        controller: 'MyCtrl'
+      }
+    }
+  })
 
   .state('app.alarmes', {
       url: '/alarmes',
+      authentificate: true,
       views: {
         'menuContent': {
           templateUrl: 'templates/alarmes.html',
@@ -62,6 +86,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
     })
     .state('app.rapport', {
       url: '/rapport',
+      authentificate: true,
       views: {
         'menuContent': {
           templateUrl: 'templates/rapport.html',
@@ -71,6 +96,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
     })
     .state('app.admin', {
       url: '/admin',
+      authentificate: true,
       views: {
         'menuContent': {
           templateUrl: 'templates/admin.html',
@@ -78,8 +104,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
         }
       }
     })
+
     .state('app.biblio', {
       url: '/biblio',
+      authentificate: true,
       views: {
         'menuContent': {
           templateUrl: 'templates/biblio.html',
@@ -97,5 +125,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'btford.socket-io','m
   //   }
   // });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/admin');
+  $urlRouterProvider.otherwise('/app/synop');
 });
